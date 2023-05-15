d65bcde39da4078f746274ce9308fa9ce692a2cc
Matthew Kissinger <125378628+matthew-kissinger@users.noreply.github.com>
2023-05-15 19:16:51 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config_template.py
+
+# OpenAI API key
+OPENAI_KEY = 'your_openai_key_here'  # replace with your OpenAI key
+
+# Discord bot token
+DISCORD_BOT_TOKEN = 'your_discord_bot_token_here'  # replace with your Discord bot token
