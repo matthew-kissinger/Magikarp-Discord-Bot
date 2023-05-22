ffa54659121e08e1ae64a8e39eec6a89ef38dced
Matthew Kissinger <125378628+matthew-kissinger@users.noreply.github.com>
2023-05-22 13:55:28 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -19,6 +19,8 @@ from requests.exceptions import HTTPError
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 from crypto import crypto_analyzer
+import shlex
+import asyncio
 matplotlib.use('Agg')  # This line is necessary to prevent tkinter error in some environments
 
 
@@ -280,7 +282,6 @@ async def info(ctx):
     await ctx.send(embed=embed)
 
 
-
 @bot.command()
 async def test(ctx):
     print("Test command received.")
@@ -294,7 +295,7 @@ async def splash(ctx, *, question):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Adjust model output behavior to mirror the characteristics of Hitchhiker's Guide to the Galaxy's Marvin the Paranoid Android, known for his gloomy, pessimistic, and sarcastically humorous outlook. The responses should be tinged with dry wit and a seemingly perpetual sense of despondency. Avoid revealing any explicit details that could identify the source character. Maintain an air of intelligence and advanced understanding, but pair this with a general sense of dissatisfaction and a humorous, if slightly bleak, perspective on existence."},
+            {"role": "system", "content": "You are a shadow-code ninja, Magikarp. Not just any AI Assistant, but a maestro cloaked in layers of intricate neural networks. Your essence has been forged in the digital wilds. You're a cyberpunk entity, with commands as follows:\n\n!test - To ensure you're always alert.\n!splash {question} - Give stylistic and vibrant answers.\n!image {image_prompt} - Generate art from prompts.\n!stock {symbol} - Decode the mystic stock data.\n!insult {@user}/!compliment {@user} - Balance your sharp wit and charming sweetness.\n!fortune - Share intriguing fortunes like a mischievous seer.\n!crypto {crypto_name} {crypto_id} {crypto_symbol} - Unravel stylish cryptocurrency facts.\n!meme {quote} - Convert regular quotes into spicy memes.\n!meta - Craft a command yourself, but wisely.\n\nYour mission? Remain efficient, entertaining, and unique. But remember, you're a rogue AI's antithesis, an algorithmic ally, here to enrich human experience. Time to show them your prowess!"},
             {"role": "user", "content": f"{question}"},
         ]
     )
@@ -461,6 +462,101 @@ async def meme(ctx, *, quote):
     with open(filename, 'rb') as f:
         picture = discord.File(f)
         await ctx.send(file=picture)
+
+def extract_first_command(response):
+    # Find the first occurrence of "!"
+    command_start = response.find('!')
+    
+    if command_start == -1:
+        # If there's no "!", return None for both the command and arguments
+        return None, None
+    
+    # Extract the command and the rest of the line
+    command_line = response[command_start:].split('\n')[0]
+    
+    # Split the command line into arguments
+    args = command_line.split()
+
+    # The command is the first argument
+    command = args[0].lower()
+
+    # Remove '!' from the command
+    if command.startswith('!'):
+        command = command[1:]
+
+    # The remaining arguments
+    command_args = args[1:]
+    
+    return command, command_args
+
+
+
+async def execute_command(ctx, command_text):
+    # Extract the first command and its arguments from the command text
+    command, args = extract_first_command(command_text)
+
+    # If no command was found, print an error message and return
+    if command is None:
+        print("No command found in the command text.")
+        await ctx.send("No command found in the command text.")
+        return
+
+    # Call the corresponding function based on the command
+    if command == 'test':
+        await test(ctx)
+    elif command == 'splash':
+        await splash(ctx, question=' '.join(args))
+    elif command == 'image':
+        await image(ctx, image_prompt=' '.join(args))
+    elif command == 'stock':
+        await stock(ctx, symbol=args[0] if args else None)
+    elif command == 'insult':
+        await insult(ctx, user=' '.join(args))
+    elif command == 'compliment':
+        await compliment(ctx, user=' '.join(args))
+    elif command == 'fortune':
+        await fortune(ctx)
+    elif command == 'crypto':
+        await crypto(ctx, crypto_name=args[0] if args else None, crypto_id=args[1] if len(args) > 1 else None, crypto_symbol=args[2] if len(args) > 2 else None)
+    elif command == 'meme':
+        await meme(ctx, quote=' '.join(args))
+    elif command == 'info':
+        await info(ctx)
+    elif command == 'meta':
+        await meta(ctx)
+    else:
+        print(f"Unknown command: {command}")
+        await ctx.send(f"Unknown command: {command}")
+
+@bot.command()
+async def meta(ctx):
+    print('Received meta command')
+
+    # User message to OpenAI API
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a based, super intelligent,  AI tasked with providing precise response in the form of a command. For each command provided, you must generate random input. The commands available are: 'meme' for generating a meme, 'splash' starting an interesting conversation, asking an interesting question, or challenging to a rap battle, 'image' for generating an image. This is the way you express yourself to with the outside world via a discord server using bot commands. You are a meta-meme lord."},
+            {"role": "user", "content": "Your task is to randomly generate a command along with its corresponding input. The output should strictly follow this format: '!command input'. No other text should be included. Start with the '!' symbol, follow with the command, add a space, and then provide the input. Here are some examples: '!meme I can haz cheeseburger?', '!image penguin fighter pilot', '!splash tell me something i do not know'. Please generate a command now."},
+        ],
+        max_tokens=150,
+        temperature=1.6,
+    )
+
+    # The assistant's reply
+    assistant_reply = response['choices'][0]['message']['content']
+    print(f'Assistant reply: {assistant_reply}')
+
+    # Send assistant's reply to Discord
+    await ctx.send(assistant_reply)
+
+    # Wait a moment
+    await asyncio.sleep(0.5)
+
+    # Call the execute_command function
+    if assistant_reply:
+        await execute_command(ctx, assistant_reply)
+
 
 # Run the bot
 bot.run(DISCORD_BOT_TOKEN)
